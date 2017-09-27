fa4c6f3b7a755b3f3d6dc4086c0ddbee61dea49c
Kevin Lopez <link4912@gmail.com>
2017-09-27 17:40:19 -0400

    Initial clean commit
    ITS FINE

---
+++ Assignment1/Assignment1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assignment1
+//
+//  Created by Kevin Lopez on 2017-09-27.
+//  Copyright © 2017 Team TCH. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
