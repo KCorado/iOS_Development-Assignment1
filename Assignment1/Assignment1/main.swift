64ba41ca6f09873334271e770af3dac2d5dfbbf5
Kevin Corado <19317703+KCorado@users.noreply.github.com>
2017-09-27 15:25:16 -0400

    Initial set up of project
    Added files automatically generated on creation of MacOS Command Line project.

---
+++ Assignment1/Assignment1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assignment1
+//
+//  Created by Xcode User on 2017-09-27.
+//  Copyright © 2017 Xcode User. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
