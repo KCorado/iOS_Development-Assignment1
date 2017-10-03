f4b2dd49f1c7672acdc19ca32832dc022d896563
Kevin Lopez <link4912@gmail.com>
2017-10-03 14:50:18 -0400

    Finished commenting files
    turns out sublime text 3 is a better swift editor than xcode is

--- Assignment1/Assignment1/Employee.swift
+++ Assignment1/Assignment1/Employee.swift
@@ -16,33 +16,38 @@ public class Employee {
 //        return self.age
 //    }
     
+    //empty constructor for employee name age and v (vehicle)
     init() {
         name = ""
         age = 0
         v = nil
     }
-    
+
+    //constructor for employee
     init (_ pName: String,_ pAge: Int) {
         name = pName
         age = pAge
         v = nil
     }
     
+    //employee constructor that has vehicle
     init (_ pName: String,_ pAge: Int, _ pV: Vehicle) {
         name = pName
         age = pAge
         v = pV
     }
     
-    
+    //function to calculate birth year (inherited by all employees)
     func calcBirthYear() -> Int {
         return (2017 - self.age)
     }
     
+    //function to calculate earnings (all employees inherit this)
     func calcEarnings() -> Double {
         return 1000.00
     }
     
+    //function to print data (all employees inherit this, will be overridden)
     func printMyData() {
         print ("Name: \(name)")
         print ("Age: \(age)")
