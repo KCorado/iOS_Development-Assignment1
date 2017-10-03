4e09ad0699fc65a97aaa30f60847cd0ac52dc4c8
Kevin Corado <19317703+KCorado@users.noreply.github.com>
2017-10-03 15:27:07 -0400

    Changes to main.swift

--- Assignment1/Assignment1/main.swift
+++ Assignment1/Assignment1/main.swift
@@ -1,45 +1,70 @@
+//--------------------------------------------//
+//Data creation for the employees to be created
 
-
+//Creation of array to store employee
 var aEmp = [Employee]()
+//-----------------------------
 
 
-// create fulltime
-var ft1 = FullTime() // default constructor
+//------Employee1 - Full-Time
+//Creating the fulltime employee object
+var ft1 = FullTime()
+
+//Adding details to the employee
 ft1.name = "Janet"
 ft1.age = 20
 ft1.salary = 50000
 ft1.bonus = 2000
 
+//Creating a vehicle for the employee, and associating it to the employee
 var vRef = Vehicle(pMake: "Ferrari",pModel: "458")
 ft1.v = vRef
 
+//Adding the fulltime employee to the array
 aEmp.append(ft1)
+//-----------------------------
 
-// create parttime
+
+//------Employee2 - Part-Time
+//Creating the parttime employee object
 var pt1 : PartTime
 
+//Creating the car to associate to the employee
 vRef = Vehicle(pMake: "Porsche", pModel: "Carrera")
 
+//Defining the employee, adding the employee's information
 pt1 = PartTime(ppName: "Matthew", ppAge: 10, pHourlyRate: 100, pNumberHoursWorked: 2, ppV: vRef)
 
+//Adding the parttime employee to the array
 aEmp.append(pt1)
+//-----------------------------
 
-//create intern
 
+//------Employee3 - Intern
+//Creating the car to associate to the employee
 vRef = Vehicle(pMake: "BMW", pModel: "X6")
+
+//Creating and defining the first intern employee, adding the employee's information
 var it1 = Intern(pName: "Loonie", pAge: 15, pSchool: "WoofCenter", ppV: vRef)
 
+//Adding the first intern employee to the array
 aEmp.append(it1)
+//-----------------------------
 
-//create intern
 
-
+//------Employee4 - Intern
+//Creating and defining the second intern employee, adding the employee's information
 var it2 = Intern(pName: "Toonie", pAge: 15, pSchool: "JKWoofCenter")
+
+//Adding the second intern employee to the array
 aEmp.append(it2)
+//-----------------------------
 
 
 
-// calculate payroll
+
+//--------------------------------------------//
+//Section to calculate the payroll for a company
 
 var totalPR : Double = 0.0
 var e : Employee
@@ -49,10 +74,10 @@ for i in 0..<aEmp.count {
     e = aEmp[i]
     var earn = e.calcEarnings()
     
-//    print ("Name: \(e.name)")
-//    print ("Age: \(e.age)")
+	//print ("Name: \(e.name)")
+	//print ("Age: \(e.age)")
     e.printMyData()
-    // display vehicle
+    //display vehicle
     if (e.v == nil) {
         print ("** Employee has not registered any vehicle ***")
     } else {
@@ -73,23 +98,10 @@ print ("TOTAL PAYROLL: \(totalPR)")
 
 
 
+/* === The Cornflakes Call to Us ===
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+▮◯
+\( )\
+ /_\
+ 
+*/
