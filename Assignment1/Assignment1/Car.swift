ee929901dbcb9742d178bbb5dc7a9088072b156d
Kevin Lopez <link4912@gmail.com>
2017-10-03 20:56:50 -0400

    Initial class commit
    Nothing special here, just the empty classes with inits for the Car and Vehicle. Still gotta figure out what exactly I want to be inherited and set by type. Ah well, there's always tomorrow.

---
+++ Assignment1/Assignment1/Car.swift
@@ -0,0 +1,13 @@
+public class Car : Vehicle{
+	var make:String
+	var plate:String
+	var feature1:String?
+	var feature2:String?
+
+	init(){
+		make = ""
+		plate=""
+		feature1=""
+		feature2=""
+	}
+}
