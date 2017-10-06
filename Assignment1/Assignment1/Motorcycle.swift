db5d2f9932dbed765aea3e20bc6ece279cc9b40d
Kevin Lopez <link4912@gmail.com>
2017-10-05 20:34:12 -0400

    Small housekeeping
    Commented some things, reimplemented Motorcycle, set up example object of motorcycle in main so we know how to do it.

--- Assignment1/Assignment1/Motorcycle.swift
+++ Assignment1/Assignment1/Motorcycle.swift
@@ -1,16 +1,24 @@
-public class Motorcycle : Vehicle{
-	var numWheels:Int
+public class Motorcycle : Vehicle {
+	public var numWheels:Int
 
-	override init(){
-		numWheels = 2
-		super.init();
-	}
-
-	init(_make:String,_plate:String,_numWheels:Int = 2){
-		numWheels = _numWheels
-        super.init(pMake: _make,pPlate: _plate);
-	}
-
+    /*
+     basically with getters and setters you need to make new
+     variables for each getter and setter and this shit's a pain
+     in the anus since its needed for
+     E V E R Y   F U C K I N G   C L A S S
+     */
+    
+    //override constructor
+    override init() {
+        numWheels = 2
+        super.init()
+    }
+    
+    override init(pMake: String, pPlate: String) {
+        self.numWheels = 2
+        super.init(pMake: pMake, pPlate: pPlate)
+    }
+    
     //overrides returnData to return generic vehicle data as well as motorcycle data
 	override func returnData() -> String {
         let t = "\(super.returnData())\nwheels: \(numWheels)"
